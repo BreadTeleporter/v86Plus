0f0e0e2c99cfbfbc69862ada352cdb1001dd2ec7
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2023-05-04 06:55:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+function waitForUrlMatch(matchString, callback) {
+  const interval = setInterval(() => {
+    if (location.href.includes(matchString)) {
+      clearInterval(interval);
+      callback();
+    }
+  }, 1000); // Check every 1 second
+}
+function main(){
+  console.log("URL matched!");
+
+  document.body.style.overflow = "hidden"
+  document.body.style.marginBottom = "50px"
+  document.getElementById("terminal").style.display = "none"
+
+  var loadedString = document.createElement("a")
+  loadedString.innerHTML = "v86+ Loaded"
+  loadedString.style.float = "right"
+  document.body.appendChild(loadedString)
+
+
+  const runtimeOptionsDiv = document.getElementById("runtime_options");
+  const childElements = runtimeOptionsDiv.querySelectorAll("*");
+  
+  childElements.forEach((element) => {
+    if (element.value === "Go fullscreen") {
+      element.style.display = "none";
+    }
+  });
+  var newInput = document.createElement("input");
+  newInput.type = "button";
+
+  // Set the value and onclick attributes of the button
+  newInput.value = "Fullscreen";
+  newInput.onclick = () => {
+    document.getElementById("vga").requestFullscreen()
+  };
+
+  var vga = document.getElementById("vga")
+  vga.removeAttribute("style");
+  vga.style = "image-rendering: pixelated"
+  // Append the new button to the runtimeOptionsDiv
+  runtimeOptionsDiv.appendChild(newInput);
+  var newInput = document.createElement("input");
+  newInput.type = "button";
+  newInput.id = "aa"
+
+  // Set the value and onclick attributes of the button
+  newInput.value = "Enable AA";
+  newInput.onclick = () => {
+    var aa = document.getElementById("aa")
+    if (aa.value === "Enable AA"){
+      document.getElementById("vga").style = "image-rendering: none"
+      aa.value = "Disable AA"
+    } else if (aa.value === "Disable AA"){
+      document.getElementById("vga").style = "image-rendering: pixelated"
+      aa.value = "Enable AA"      
+    }
+    
+  };
+
+  // Append the new button to the runtimeOptionsDiv
+  runtimeOptionsDiv.appendChild(newInput);
+}
+if (location.href.includes("https://copy.sh/v86/?profile=")){
+  main()
+} else{
+  waitForUrlMatch("https://copy.sh/v86/?profile=", () => {
+    main()
+  });
+}
